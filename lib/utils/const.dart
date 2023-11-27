6de6beeb283dbe01143eafe4026012e7c2045f03
Thanos <iamthegreatthanos@gmail.com>
2023-11-27 14:58:09 +0600

    Edited url address.

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   static const String appPassword = '123456';
-  static const String baseUrl = 'http://backvisits.cardioservice.com.kz/';
+  static const String baseUrl = 'https://backvisits.cardioservice.com.kz/';
 }
